ec5c91d4195fc401a67a73ce635daccd033a38ef
yamen haddad <yamenhaddad1994@gmail.com>
2020-05-10 02:11:41 +0200

    Generate array of permutation of size n randomly positioned

--- bar-chart.py
+++ bar-chart.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 def bubbleSort(n,a):
     for i in range(1,n):
@@ -9,12 +10,19 @@ def bubbleSort(n,a):
                 a[j] = a[j+1]
                 a[j+1] = tmp
     return a
-
-n = int(input())
+#reading the array from std input 
+'''n = int(input())
 s = input().split()
 a = list()
 for i in range(n):
-    a.append(int(s[i]))
+    a.append(int(s[i]))'''
+# Generating array of permutation of size n randomly positioned in the array
+n = int(input())
+a = [i for i in range(1,n+1)]
+#print(a)
+#shuffle a in place
+random.shuffle(a)
+#print(a)
 plt.figure("array")
 xmin = 0
 xmax = n
